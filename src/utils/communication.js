365806cd11b0ca700733c65e7b3867a1dfdce316
loyalperson <loyalman00715@outlook.com>
2023-10-17 08:26:54 -0500

    change domain

--- src/utils/communication.js
+++ src/utils/communication.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const sendQuery = async (data) => {
 
-  const response = await axios.post('http://193.178.210.10:5000/api/search',data);
+  const response = await axios.post('https://8735-193-178-210-10.ngrok-free.app/api/search',data);
 
   return response;
 };
 
 export const getFile = async (data) => {
-  const response = await axios.post('http://193.178.210.10:5000/api/search/file',data,{responseType: 'arraybuffer'});
+  const response = await axios.post('https://8735-193-178-210-10.ngrok-free.app/api/search/file',data,{responseType: 'arraybuffer'});
   return response;
 };
